947dd8602f028625f2439e54dc4a6c6547f330f9
Samantha Remmey <sqr8ap@virginia.edu>
2024-07-02 11:13:59 -0400

    Installed locally and pushing changes

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     url = 'https://github.com/sqr8ap/Sample-Package',
     license = 'MIT',
     description = 'A package!',
-    long_description = open('README.txt').read()
+    long_description = open('README.md').read()
 )
